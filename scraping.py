cbf82daaded5a4fe1d0c69ecb9081cb5fb12f83b
kristianQh <hgj828@alumni.ku.dk>
2023-11-28 23:38:44 +0100

    Synthetic data generation w/token bucket

--- scraping.py
+++ scraping.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from langdetect import detect
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import TimeoutException
+from tqdm import tqdm
 
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -42,33 +43,40 @@ def scrape_jobindex(job_keywords):
     base_url = "https://www.jobindex.dk/jobsoegning?page={page}&q={job}%27&subid=1&subid=3&subid=4&subid=6&subid=93"
     job_listings = retrieve_jobs(job_keywords, "div", {"class": ["PaidJob", "jix_robotjob"]}, base_url)
     
-    for job_ad in job_listings:
+    for job_ad in tqdm(job_listings):
         url = job_ad.find("a", text='Se jobbet')["href"]
         if url in jobs:
             continue
         title = find_element(job_ad, "h4", {"class": ""})
-        print(title)
         company = find_element(job_ad, "div", {"class": "jix-toolbar-top__company"})
         location = find_element(job_ad, "span", {"class": "jix_robotjob--area"})
         description = find_description(url, None)
+        if description == "Unknown":
+            continue
         lang = detect(description)
-        jobs.append([title, company, location, description, lang])
+        if lang == "da":
+            continue
+        jobs.append([title, company, location, url, description, lang])
     return jobs
 
 def scrape_hub(job_keywords):
     base_url = "https://thehub.io/jobs?search={job}&location=Denmark&countryCode=DK&sorting=mostPopular&page={page}"
     job_listings = retrieve_jobs(job_keywords, "div", {"class": "my-10"}, base_url)
     
-    for job_ad in job_listings:
+    for job_ad in tqdm(job_listings):
         url = "https://thehub.io" + job_ad.find("a", class_="card-job-find-list__link")["href"]
         if url in jobs:
             continue
         title = find_element(job_ad, "span", {"class": "card-job-find-list__position"})
         bullets = job_ad.find("div", {"class": "bullet-inline-list"})
         company, location = [span.text for span in bullets.find_all("span")][:2]
-        description = find_description(url, {'tag': 'div', 'class': {'class': 'jix-toolbar-top__company'}})
+        description = find_description(url, {"tag": "div", "class": {"class": "text-block"}})
+        if description == "Unknown":
+            continue
         lang = detect(description)
-        jobs.append([title, company, location, description, lang])
+        if lang == "da":
+            continue
+        jobs.append([title, company, location, url, description, lang])
     return jobs
 
 def find_element(soup, tag, class_):
@@ -79,7 +87,7 @@ def find_description(url, desc_config):
     driver.set_page_load_timeout(3)
     try:
         driver.get(url)
-        time.sleep(0.5)
+        time.sleep(1)
         html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
         # print(soup)
@@ -94,7 +102,8 @@ def find_description(url, desc_config):
         return "Unknown"
 
 
-scraped_jobs = scrape_jobindex(["\"Data+Science\""])
+scraped_jobs = scrape_jobindex(["\"Data+Science\"", "\"Machine+Learning\"", "\"Computer+Science\"", "\"Software+Developer\""])
+scraped_jobs = scrape_hub(["\"Data Science\"", "\"Machine Learning\""])
 print(len(scraped_jobs))
 driver.quit()
 
